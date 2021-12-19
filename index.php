9b1b43422dba984f771bc3acbb3559c8986abb50
Aurora Grippaudo <aurora268@hotmail.it>
2021-12-19 14:11:49 +0100

    Snack 4

--- index.php
+++ index.php
@@ -128,3 +128,19 @@ for($j=0; $j < count($posts[$date[$i]]); $j++){
 };
 ?>
 
+<!-- Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php $casualArray = []; 
+    
+    while(count($casualArray)<= 15){
+    $number = rand(1,100);
+
+    if(!in_array($number, $casualArray)){
+        $casualArray[] = $number;
+    };
+    };
+
+    print_r($casualArray);
+?>
+
